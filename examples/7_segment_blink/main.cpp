87e5dea5594d1313efff5568a58c602c5cca2908
Alan Ludwig <al@theludwigfamily.com>
2019-11-07 19:48:04 -0800

    CPP port of the Adafruit LED backpack (#1)
    * Still not building cpp
    
    * Get .cpp files building in cmake
    
    * c++ 17

---
+++ examples/7_segment_blink/main.cpp
@@ -0,0 +1,45 @@
+/* Copyright (c) Microsoft Corporation. All rights reserved.
+   Licensed under the MIT License. */
+
+// This minimal Azure Sphere app repeatedly toggles GPIO 8, which is the red channel of RGB
+// LED 1 on the MT3620 RDB. Use this app to test that device and SDK installation succeeded
+// that you can build, deploy, and debug a CMake app with Visual Studio.
+//
+// It uses the API for the following Azure Sphere application libraries:
+// - log (messages shown in Visual Studio's Device Output window during debugging)
+
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <applibs/log.h>
+
+#include "../../Adafruit_LEDBackpack.h"
+
+int main(void)
+{
+    Log_Debug("Starting 7-Segment Blink application...\n");
+	
+    Adafruit_7segment sevseg(0); 
+
+    const struct timespec sleepTime = {1, 0};
+	sevseg.begin(0x70);
+
+    while (true) {
+		//print_long_Adafruit_7segment(&sevseg, 8888, 10);
+		sevseg.writeDigitNum(0, 8, true);
+		sevseg.writeDigitNum(1, 8, true);
+		sevseg.drawColon(true);
+		sevseg.writeDigitNum(3, 8, true);
+		sevseg.writeDigitNum(4, 8, true);
+		sevseg.writeDisplay(); 
+
+		nanosleep(&sleepTime, NULL);
+
+		sevseg.clear();
+		sevseg.writeDisplay(); 
+		
+		nanosleep(&sleepTime, NULL);
+    }
+
+    return 0;
+}
---
+++ examples/7_segment_blink/run_cmake.bat
@@ -0,0 +1,13 @@
+@echo on
+mkdir build
+cd build
+
+cmake ^
+-G "Ninja" ^
+-DCMAKE_INSTALL_PREFIX:PATH="." ^
+-DCMAKE_TOOLCHAIN_FILE="Toolchain.cmake" ^
+-DAZURE_SPHERE_TARGET_API_SET="3" ^
+--no-warn-unused-cli ^
+-DCMAKE_BUILD_TYPE="Debug" ^
+-DCMAKE_MAKE_PROGRAM="ninja.exe" ^
+..
