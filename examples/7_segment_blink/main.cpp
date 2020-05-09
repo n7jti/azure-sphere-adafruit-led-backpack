7d203cb07ca65bd33d61bd5bb99231f378eba43d
Alan Ludwig <al@theludwigfamily.com>
2020-05-09 09:10:17 -0700

    Updates to build for sample

--- examples/7_segment_blink/main.cpp
+++ examples/7_segment_blink/main.cpp
@@ -1,12 +1,15 @@
 /* Copyright (c) Microsoft Corporation. All rights reserved.
    Licensed under the MIT License. */
 
-// This minimal Azure Sphere app repeatedly toggles GPIO 8, which is the red channel of RGB
-// LED 1 on the MT3620 RDB. Use this app to test that device and SDK installation succeeded
+// This minimal Azure Sphere app repeatedly toggles GPIO 8, which is the red
+// channel of RGB
+// LED 1 on the MT3620 RDB. Use this app to test that device and SDK
+// installation succeeded
 // that you can build, deploy, and debug a CMake app with Visual Studio.
 //
 // It uses the API for the following Azure Sphere application libraries:
-// - log (messages shown in Visual Studio's Device Output window during debugging)
+// - log (messages shown in Visual Studio's Device Output window during
+// debugging)
 
 #include <errno.h>
 #include <string.h>
@@ -15,45 +18,47 @@
 
 #include "../../Adafruit_LEDBackpack.h"
 
-extern "C" void __cxa_pure_virtual() { while (1); }
+extern "C" void __cxa_pure_virtual() {
+  while (1)
+    ;
+}
 
 using adafruit::ledbackpack::Adafruit_7segment;
 
-int main(void)
-{
-    Log_Debug("Starting 7-Segment Blink application...\n");
-	
-    Adafruit_7segment sevseg(0); 
+int main(void) {
+  Log_Debug("Starting 7-Segment Blink application...\n");
 
-    const struct timespec sleepTime = {1, 0};
-	sevseg.begin(0x70);
+  Adafruit_7segment sevseg(0);
 
-    while (true) {
-		//print_long_Adafruit_7segment(&sevseg, 8888, 10);
-		sevseg.writeDigitNum(0, 8, true);
-		sevseg.writeDigitNum(1, 8, true);
-		sevseg.drawColon(true);
-		sevseg.writeDigitNum(3, 8, true);
-		sevseg.writeDigitNum(4, 8, true);
-		sevseg.writeDisplay(); 
+  const struct timespec sleepTime = {1, 0};
+  sevseg.begin(0x70);
 
-		nanosleep(&sleepTime, nullptr);
+  while (true) {
+    // print_long_Adafruit_7segment(&sevseg, 8888, 10);
+    sevseg.writeDigitNum(0, 8, true);
+    sevseg.writeDigitNum(1, 8, true);
+    sevseg.drawColon(true);
+    sevseg.writeDigitNum(3, 8, true);
+    sevseg.writeDigitNum(4, 8, true);
+    sevseg.writeDisplay();
 
-		sevseg.clear();
-		sevseg.writeDisplay(); 
+    nanosleep(&sleepTime, nullptr);
 
-		nanosleep(&sleepTime, nullptr);
+    sevseg.clear();
+    sevseg.writeDisplay();
 
-		sevseg.print(1234);
-		sevseg.writeDisplay();
-		
-		nanosleep(&sleepTime, nullptr);
+    nanosleep(&sleepTime, nullptr);
 
-		sevseg.clear();
-		sevseg.writeDisplay();
+    sevseg.print(1234);
+    sevseg.writeDisplay();
 
-		nanosleep(&sleepTime, nullptr);
-    }
+    nanosleep(&sleepTime, nullptr);
 
-    return 0;
+    sevseg.clear();
+    sevseg.writeDisplay();
+
+    nanosleep(&sleepTime, nullptr);
+  }
+
+  return 0;
 }
